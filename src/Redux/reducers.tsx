28d15e1f099832ad7930519772ad813f40d56426
Ahmed Rizk <ahmed.rezk.fe@gmail.com>
2023-01-27 14:01:26 +0200

    edit result search

--- src/Redux/reducers.tsx
+++ src/Redux/reducers.tsx
@@ -6,7 +6,7 @@ const INTIALSTATE = {
 export const cardReducer = (state = INTIALSTATE, action) => {
   switch (action.type) {
     case ActionTypes.START_APP:
-      return{cardItems : action.payload , resultNum : action.payload.length }
+      return{cardItems : action.payload , resultNum : action.payload?.length }
     default:
       return state;
   }
